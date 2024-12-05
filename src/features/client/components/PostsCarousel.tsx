a48d832c768f19c985689855c322fabb0b3ebc32
Tomaatti <tuomas49@gmail.com>
2024-12-05 16:07:00 +0200

    Removed the animating. Also added button to show comments

--- src/features/client/components/PostsCarousel.tsx
+++ src/features/client/components/PostsCarousel.tsx
@@ -1,70 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
-  CarouselIndicators, CarouselCaption, Button
-} from "reactstrap"
-import type { IPost } from "../../../types/IPost"
-import { FaExclamationTriangle } from "react-icons/fa"
-import OverlayComponent from "../utils/Overlay"
-import Comment from "./Comment"
-import { selectPostComments } from "../../../store/redditSlice"
-import { useAppSelector } from "../../../store/hooks"
-
+  CarouselIndicators,
+  CarouselCaption,
+  Button,
+} from "reactstrap";
+import { FaExclamationTriangle } from "react-icons/fa";
+import OverlayComponent from "../utils/Overlay";
+import Comment from "./Comment";
+import { selectPostComments } from "../../../store/redditSlice";
+import { useAppSelector } from "../../../store/hooks";
+import { IPost } from "../../../types/IPost"
 
 interface CarouselProps {
   posts: IPost[];
   onToggleComments: (i: number, permalink: string) => void;
 }
 
-export default function PostsCarousel({ posts, onToggleComments, ...args }: CarouselProps) {
-
-  const [activeIndex, setActiveIndex] = useState(0)
-  const [animating, setAnimating] = useState(false)
-  const [isValidImage, setIsValidImage] = useState(true)
-  const [overlayIsOpen, setOverlayIsOpen] = useState(false)
-  const [selectedPostIndex, setSelectedPostIndex] = useState<number | null>(null)
+export default function PostsCarousel({ posts, onToggleComments }: CarouselProps) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [overlayIsOpen, setOverlayIsOpen] = useState(false);
   const comments = useAppSelector(
-    selectedPostIndex !== null ? selectPostComments(selectedPostIndex) : () => ({
-      data: [],
-      isLoading: false,
-      error: false
-    })
-  )
+    selectPostComments(activeIndex)
+  );
+
+  const toggleOverlay = () => setOverlayIsOpen(!overlayIsOpen);
+
+  const showComments = () => {
+    const permalink = posts[activeIndex].permalink;
+    onToggleComments(activeIndex, permalink);
+    toggleOverlay();
+  };
+
   const next = () => {
-    if (animating) return
-    const nextIndex = activeIndex === posts.length - 1 ? 0 : activeIndex + 1
-    setActiveIndex(nextIndex)
-  }
+    const nextIndex = activeIndex === posts.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
 
   const previous = () => {
-    if (animating) return
-    const nextIndex = activeIndex === 0 ? posts.length - 1 : activeIndex - 1
-    setActiveIndex(nextIndex)
-  }
+    const nextIndex = activeIndex === 0 ? posts.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
 
-  const goToIndex = (newIndex: number) => {
-    if (animating) return
-    setActiveIndex(newIndex)
-  }
+  const handleImageError = () => {};
 
-  const isImageUrl = (url) => {
-    const imageExtensions = /\.(jpg|jpeg|png|gif|bmp|webp|svg)$/i
-    return imageExtensions.test(url)
-  }
-
-  const handleImageError = () => {
-    setIsValidImage(false)
-  }
-
-  const toggleOverlay = () => setOverlayIsOpen(!overlayIsOpen)
-
-  const showComments = (i: number, permalink: string) => {
-    onToggleComments(i, permalink)
-    setSelectedPostIndex(i)
-    toggleOverlay()
-  }
+  const isImageUrl = (url: string) =>
+    /\.(jpg|jpeg|png|gif|bmp|webp|svg)$/i.test(url);
 
   return (
     <>
@@ -72,90 +55,62 @@ export default function PostsCarousel({ posts, onToggleComments, ...args }: Caro
         activeIndex={activeIndex}
         next={next}
         previous={previous}
-        light
-        {...args}
-
+        interval={false}
       >
         <CarouselIndicators
           items={posts.map((_, i) => ({ id: i }))}
           activeIndex={activeIndex}
-          onClickHandler={goToIndex}
+          onClickHandler={(index) => setActiveIndex(index)}
         />
-        {posts?.map((post, i) => (
+        {posts.map((post, i) => (
           <CarouselItem
-            onExiting={() => setAnimating(true)}
-            onExited={() => setAnimating(false)}
             key={i}
           >
-            {isValidImage && isImageUrl(post.url) ? (
+            {isImageUrl(post.url) ? (
               <img
                 src={post.url}
                 alt={post.title}
-                style={{
-                  width: "100%",
-                  height: "70vh",
-                  margin: "auto"
-                }}
                 className="d-block w-100 rounded-5"
+                style={{ height: "70vh", objectFit: "cover" }}
                 onError={handleImageError}
               />
             ) : (
               <div
-                className="d-flex justify-content-center align-items-center bg-secondary-subtle rounded-5"
-                style={{
-                  width: "100%",
-                  height: "70vh",
-                  margin: "auto"
-                }}
+                className="d-flex justify-content-center align-items-center bg-secondary rounded-5"
+                style={{ height: "70vh" }}
               >
-                <FaExclamationTriangle
-                  style={{
-                    fontSize: "10vw",
-                    color: "red"
-                  }}
-                />
+                <FaExclamationTriangle style={{ fontSize: "10vw", color: "red" }} />
                 <div className="text-muted ms-2">Image not available</div>
               </div>
             )}
-            <CarouselCaption
-              captionText={post.title}
-              captionHeader={post.title}
-            />
-            <Button
-              color="primary"
-              onClick={() => showComments(i, post.permalink)}
-              className="mt-2"
-            >
-              Comments
-            </Button>
+            <CarouselCaption captionHeader={post.title} captionText={post.title} />
           </CarouselItem>
         ))}
-        <CarouselControl
-          direction="prev"
-          directionText="Previous"
-          onClickHandler={previous}
-        />
-        <CarouselControl
-          direction="next"
-          directionText="Next"
-          onClickHandler={next}
-        />
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
       </Carousel>
-      <OverlayComponent
-        isOpen={overlayIsOpen}
-        toggle={toggleOverlay}
-        title={'Comments'}
-      >
-        {comments?.isLoading ? (
+      <div className="d-flex justify-content-center mt-3">
+        <Button
+          block
+          color="info"
+          outline
+          className='rounded-3'
+          onClick={showComments}>
+          Show Comments
+        </Button>
+      </div>
+      {/* Overlay for Comments */}
+      <OverlayComponent isOpen={overlayIsOpen} toggle={toggleOverlay} title="Comments">
+        {comments.isLoading ? (
           <div>Loading comments...</div>
-        ) : comments?.error ? (
+        ) : comments.error ? (
           <div>Failed to load comments.</div>
-        ) : comments?.data?.length > 0 ? (
+        ) : comments.data?.length > 0 ? (
           comments.data.map((comment, i) => <Comment key={i} comment={comment} />)
         ) : (
           <div>No comments available.</div>
         )}
       </OverlayComponent>
     </>
-  )
+  );
 }
